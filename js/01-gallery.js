2154234b1dd0ac447f5a235e4c48895f7e1d8a6c
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-06-23 23:31:20 +0300

    First hw complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// variable for lightbox instance
+let lightboxImage;
+
+// create markup gallery
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', createMarkupInnerGallery(galleryItems));
+
+// event listener on gallery
+galleryContainer.addEventListener('click', function(e) {
+  // cansel page reload
+  e.preventDefault();
+  
+  // create lightbox instance for target clickable element
+  lightboxImage = createBigImage(e.target.dataset.source);
+  // show lightbox instance and callback create keydown event listener
+  lightboxImage.show(createKeyDownEventListener);
+});
+
+
+// create markup for each gallery object and join them in one string
+function createMarkupInnerGallery(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `
+  }).join("");
+}
+
+// create Lightbox library object
+function createBigImage(bigImgSource) {
+  const bigImg = basicLightbox.create(`<img class="modal-image" src="${bigImgSource}">`);
+  return bigImg;
+}
+
+// create keydown event listener
+function createKeyDownEventListener() {
+  document.addEventListener("keydown", onKeyDownEscape);
+}
+
+// remove keydown event listener
+function removeKeyDownEventListener() {
+  document.removeEventListener("keydown", onKeyDownEscape);
+}
+
+// check lightbox instance and pressed Escape button
+// close lightbox instance and callback remove keydown event listener from this instance
+function onKeyDownEscape(e) {
+  if(e.code === 'Escape' && lightboxImage.visible()){
+    lightboxImage.close(removeKeyDownEventListener);
+  }
+}
