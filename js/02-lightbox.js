1bae665d2f79c8f502d6a47d5385732d0545e6ce
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-06-24 00:13:05 +0300

    Second hw complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// create markup gallery
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', createMarkupInnerGallery(galleryItems));
+
+
+galleryContainer.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  // create instance of SimpleLihtBox and set options
+  const lightbox = new SimpleLightbox('.gallery__item', {
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+});
+
+
+// create markup for each gallery object and join them in one string
+function createMarkupInnerGallery(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `
+  }).join("");
+}
